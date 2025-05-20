55b64438aa12127675f491a17f4d17ba9dcc9d65
vinicius <vinicius.neves@al.infnet.edu.br>
2025-05-20 14:44:33 -0300

    Corrigindo erros

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -87,6 +88,105 @@ int main()
       printf(" %d ", tabuleiro[i][j]);
     }
     printf("\n");
+  }
+
+  // Matrizes de Habilidades
+  int cone[5][5] = {0};
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 5 / 2 - i; j <= 5 / 2 + i; j++)
+    {
+      if (j >= 0 && j < 5)
+        cone[i][j] = 1;
+    }
+  }
+
+  int cruz[5][5] = {0};
+  for (int i = 0; i < 5; i++)
+  {
+    cruz[5 / 2][i] = 1; // Linha central
+    cruz[i][5 / 2] = 1; // Coluna central
+  }
+
+  int octaedro[5][5] = {0};
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      if (abs(i - 5 / 2) + abs(j - 5 / 2) <= 5 / 2)
+      {
+        octaedro[i][j] = 1;
+      }
+    }
+  }
+
+  // Aplicação das Habilidades no Tabuleiro
+  int origemLinha, origemColuna;
+  // Cone no caso, (2,2)
+  origemLinha = 2;
+  origemColuna = 2;
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      if (cone[i][j] == 1)
+      {
+        int linhaTabuleiro = origemLinha + i - 5 / 2;
+        int colunaTabuleiro = origemColuna + j - 5 / 2;
+        if (linhaTabuleiro >= 0 && linhaTabuleiro < 10 && colunaTabuleiro >= 0 && colunaTabuleiro < 10)
+        {
+          if (tabuleiro[linhaTabuleiro][colunaTabuleiro] == 0)
+          {
+            tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+          }
+        }
+      }
+    }
+  }
+
+  // cruz, no caso (5,5)
+
+  origemLinha = 5;
+  origemColuna = 5;
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      if (cruz[i][j] == 1)
+      {
+        int linhaTabuleiro = origemLinha + i - 5 / 2;
+        int colunaTabuleiro = origemColuna + j - 5 / 2;
+        if (linhaTabuleiro >= 0 && linhaTabuleiro < 10 && colunaTabuleiro >= 0 && colunaTabuleiro < 10)
+        {
+          if (tabuleiro[linhaTabuleiro][colunaTabuleiro] == 0)
+          {
+            tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+          }
+        }
+      }
+    }
+  }
+
+  // octaedro, no caso (7,7)
+  origemLinha = 7;
+  origemColuna = 7;
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      if (octaedro[i][j] == 1)
+      {
+        int linhaTabuleiro = origemLinha + i - 5 / 2;
+        int colunaTabuleiro = origemColuna + j - 5 / 2;
+        if (linhaTabuleiro >= 0 && linhaTabuleiro < 10 && colunaTabuleiro >= 0 && colunaTabuleiro < 10)
+        {
+          if (tabuleiro[linhaTabuleiro][colunaTabuleiro] == 0)
+          {
+            tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+          }
+        }
+      }
+    }
   }
 
   return 0;
